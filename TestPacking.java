67c65536402318c9b3d0b549c6bbb954dd0cf3f1
Mauer-Oats <maueroats@gmail.com>
2021-02-12 16:44:38 -0600

    IDEA files. Changes to make tests compile (not valid yet).

--- TestPacking.java
+++ TestPacking.java
@@ -10,11 +10,8 @@ public class TestPacking {
     List<Integer> ex2;
     @Before
     void setup_examples() {
-        int[] items1 = {10,5,6,11,4,3};
-        ex1 = new ArrayList<>(Arrays.asList(items1));
-
-        int[] items2 = {10,12,3,6,4,11,3};
-        ex2 = new ArrayList<>(Arrays.asList(items2));
+        ex1 = new ArrayList<Integer>(Arrays.asList(10,5,6,11,4,3));
+        ex2 = new ArrayList<Integer>(Arrays.asList(10,12,3,6,4,11,3));
     }
     @Test
     public void test_nextFit_1() {
@@ -22,21 +19,7 @@ public class TestPacking {
         NextFit fitter = new NextFit();
         List<Integer> partial = Arrays.asList(1,1,2,2);
         List<Integer> correct = Arrays.asList(1,1,2,2,3,3);
-        ArrayList<Integer> bins = new ArrayList<>();
-        List<Integer> answer;
-        answer = fitter.itemPlace(bins, binCapacity, items1.remove(0));
-        List<Integer> answer = fitter.itemPlace(bins, binCapacity, items1.remove(0));
-        List<Integer> answer = fitter.itemPlace(bins, binCapacity, items1.remove(0));
-        List<Integer> answer = fitter.itemPlace(bins, binCapacity, items1.remove(0));
-        assertEquals(correct, partial, "NextFit 1.1");  
-        assertEquals(correct, answer, "NextFit 1.1");  
-    }
-    @Test
-    public void test_nextFit_2() {
-        NextFit fitter = new NextFit();
-        List<Integer> correct = Arrays.asList(1,1,2,2,3,3);
-        ArrayList<Integer> bins = new ArrayList<>();
-        List<Integer> answer = fitter.itemPlace(bins, binCapacity, itemSize);
-        assertEquals(correct, answer, "NextFit on example 1");  
+        int itemSize = 20; // fixme
+        int loc = fitter.itemPlace(partial, binCapacity, itemSize);
     }
 }
