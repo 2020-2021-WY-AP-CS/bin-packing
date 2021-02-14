2fb84a694e33e39e957d9e8642b74b57325cc87e
Mauer-Oats <maueroats@users.noreply.github.com>
2021-02-13 18:07:42 -0600

    Point to PackingStrategy.java for information.

--- NextFit.java
+++ NextFit.java
@@ -1,5 +1,6 @@
 import java.util.*;
 /*
+Read PackingStrategy.java for a description of the function.
 See the description of the "next fit" strategy. 
 Should return the bin number where the item is to be placed.
 */
