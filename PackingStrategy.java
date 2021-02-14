7ff2ea812d99dc9ca90e30d7577cf31efeb61bac
Mauer-Oats <maueroats@users.noreply.github.com>
2021-02-13 18:01:00 -0600

    Clarify meaning of parameters
    Added a purpose and parameter descriptions.

--- PackingStrategy.java
+++ PackingStrategy.java
@@ -1,5 +1,11 @@
 import java.util.*;
-
+/**
+ * Identify the bin in which an itemSize item should be placed.
+ * Does not alter its inputs.
+ * @param bins a list containing the current quantity stored in each bin
+ * @param binCapacity the maximum capacity of each bin
+ * @param itemsize the size if the item to be placed
+ */
 public interface PackingStrategy {
     public int itemPlace(List<Integer> bins,
                          int binCapacity,
